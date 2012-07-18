cd676afcaeefd703cc0314ba92135f20ac3ae6a6
mellowaredev <mellowaredev@gmail.com>
2012-07-18 23:53:28 +0000

    Added License header to Java files and changed order of Control Menu for small devices like mine.  Formatted XML.

--- src/org/tunesremote/util/ClickSpan.java
+++ src/org/tunesremote/util/ClickSpan.java
@@ -1,3 +1,27 @@
+/*
+    TunesRemote+ - http://code.google.com/p/tunesremote-plus/
+    
+    Copyright (C) 2008 Jeffrey Sharkey, http://jsharkey.org/
+    Copyright (C) 2010 TunesRemote+, http://code.google.com/p/tunesremote-plus/
+    
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    
+    The Initial Developer of the Original Code is Jeffrey Sharkey.
+    Portions created by Jeffrey Sharkey are
+    Copyright (C) 2008. Jeffrey Sharkey, http://jsharkey.org/
+    All Rights Reserved.
+ */
 package org.tunesremote.util;
 
 import org.tunesremote.LibraryActivity;
@@ -12,55 +36,57 @@ import android.view.View;
 import android.widget.TextView;
 
 /**
- * Uses a fudge method to allow us to override clicks in a text view. Used in {@link LibraryActivity}. 
- * I found this on StackOverflow at http://stackoverflow.com/questions/1697084/handle-textview-link-click-in-my-android-app
- * so I cannot take the credit for this
+ * Uses a fudge method to allow us to override clicks in a text view. Used in
+ * {@link LibraryActivity}. I found this on StackOverflow at
+ * http://stackoverflow
+ * .com/questions/1697084/handle-textview-link-click-in-my-android-app so I
+ * cannot take the credit for this
  * @author Jonathan S.
- *
  */
 public class ClickSpan extends ClickableSpan {
 
-	private OnClickListener mListener;
+   private final OnClickListener mListener;
 
-	public ClickSpan(OnClickListener listener) {
-		mListener = listener;
-	}
+   public ClickSpan(OnClickListener listener) {
+      mListener = listener;
+   }
 
-	@Override
-	public void onClick(View widget) {
-		if (mListener != null) mListener.onClick();
-	}
+   @Override
+   public void onClick(View widget) {
+      if (mListener != null)
+         mListener.onClick();
+   }
 
-	public interface OnClickListener {
-		/**
-		 * What to do when a link is clicked.
-		 */
-		void onClick();
-	}
-	
-	public static void clickify(TextView view, final String clickableText, 
-		    final ClickSpan.OnClickListener listener) {
+   public interface OnClickListener {
+      /**
+       * What to do when a link is clicked.
+       */
+      void onClick();
+   }
 
-		    CharSequence text = view.getText();
-		    String string = text.toString();
-		    ClickSpan span = new ClickSpan(listener);
+   public static void clickify(TextView view, final String clickableText, final ClickSpan.OnClickListener listener) {
 
-		    int start = string.indexOf(clickableText);
-		    int end = start + clickableText.length();
-		    if (start == -1) return;
+      CharSequence text = view.getText();
+      String string = text.toString();
+      ClickSpan span = new ClickSpan(listener);
 
-		    if (text instanceof Spannable) {
-		        ((Spannable)text).setSpan(span, start, end, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-		    } else {
-		        SpannableString s = SpannableString.valueOf(text);
-		        s.setSpan(span, start, end, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-		        view.setText(s);
-		    }
+      int start = string.indexOf(clickableText);
+      int end = start + clickableText.length();
+      if (start == -1)
+         return;
 
-		    MovementMethod m = view.getMovementMethod();
-		    if ((m == null) || !(m instanceof LinkMovementMethod)) {
-		        view.setMovementMethod(LinkMovementMethod.getInstance());
-		    }
-	}
+      if (text instanceof Spannable) {
+         ((Spannable) text).setSpan(span, start, end, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+      } else {
+         SpannableString s = SpannableString.valueOf(text);
+         s.setSpan(span, start, end, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+         view.setText(s);
+      }
+
+      MovementMethod m = view.getMovementMethod();
+      if ((m == null) || !(m instanceof LinkMovementMethod)) {
+         view.setMovementMethod(LinkMovementMethod.getInstance());
+      }
+   }
 
 }
